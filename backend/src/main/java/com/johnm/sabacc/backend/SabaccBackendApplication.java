0144ed824cf601ec8d6b80ad77043932a2d29ffe
John McNally <Jmcnally1@sheffield.ac.uk>
2025-12-25 15:54:49 +0000

    Initial commit

---
+++ backend/src/main/java/com/johnm/sabacc/backend/SabaccBackendApplication.java
@@ -0,0 +1,13 @@
+package com.johnm.sabacc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabaccBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabaccBackendApplication.class, args);
+	}
+
+}
